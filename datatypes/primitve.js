873ce272a8a2f507a84525bf64b76423e75167bd
Jain-Daksh <daksh44jain@gmail.com>
2022-12-01 12:54:29 +0530

    datatypes

---
+++ datatypes/primitve.js
@@ -0,0 +1,29 @@
+//strings
+
+var name = "dj"
+console.log("strings " + name)
+console.log("data type " + (typeof name) )
+
+
+//number
+var num = 4
+console.log("number " + num)
+console.log("data type " + (typeof num) )
+
+//boolean
+
+let iscar = false
+console.log("boolean " + iscar)
+console.log("data type " + (typeof iscar) )
+
+//null
+
+let nullval = null
+console.log("null " + nullval)
+console.log("data type " + (typeof nullval) )
+
+//undefined 
+
+let undef = undefined
+console.log("undefined " + undef)
+console.log("data type " + (typeof undef) )
---
+++ datatypes/reference.js
@@ -0,0 +1,26 @@
+//arr
+
+myarr = [1,2,3,4]
+console.log("array " + myarr)
+console.log("data type " + (typeof myarr) )
+
+//function
+
+function sum(a,b) {
+  let  c = a+b
+  console.log("sum " + c)
+}
+sum(1,2)
+
+console.log("data type " + (typeof sum) )
+
+//object literals
+
+let marks = {
+  a: 12,
+  b: 23,
+  c: 30
+}
+
+console.log("object " + marks)
+console.log("data type " + (typeof marks) )
